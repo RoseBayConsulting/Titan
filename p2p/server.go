01ace59f484e9100816f2c82dea00e61cd1298a4
Gopal Ojha <gopalonevirus@gmail.com>
2017-12-18 12:23:34 +0545

    elliminating waiting in server response
    used for testing previously

--- p2p/server.go
+++ p2p/server.go
@@ -728,18 +728,18 @@ func (srv *Server) SetupConn(fd net.Conn, flags connFlag, dialDest *discover.Nod
 //starting Rose Premissioning
 
 currentNode :=srv.NodeInfo().ID
-fmt.Println(currentNode)
+//fmt.Println(currentNode)
 
-fmt.Print("Press 'Enter' to continue...")
-bufio.NewReader(os.Stdin).ReadBytes('\n') 
+//fmt.Print("Press 'Enter' to continue...")
+//bufio.NewReader(os.Stdin).ReadBytes('\n') 
 
 //duration:=time.Second
 //time.Sleep(duration)
-cnodeName :=srv.NodeInfo().Name
-fmt.Println(cnodeName)
+//cnodeName :=srv.NodeInfo().Name
+//fmt.Println(cnodeName)
 
 fmt.Print("Press 'Enter' to continue...")
-bufio.NewReader(os.Stdin).ReadBytes('\n') 
+//bufio.NewReader(os.Stdin).ReadBytes('\n') 
 
 
 
